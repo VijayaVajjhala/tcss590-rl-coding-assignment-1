491f3646e76e74a83d8c9ac16eba9de743cb11f8
VijayaVajjhala <vijayabharati.vajjhala@gmail.com>
2026-05-03 15:41:54 -0700

    initial commit

---
+++ bc.py
@@ -0,0 +1,63 @@
+"""
+TODO: MODIFY TO FILL IN YOUR BC IMPLEMENTATION
+"""
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import numpy as np
+from utils import rollout
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+def simulate_policy_bc(env, policy, expert_data, num_epochs=500, episode_length=50,
+                       batch_size=32):
+
+    # Fill in your BC implementation in this function.
+
+    # Hint: Just flatten your expert dataset and use standard pytorch supervised learning code to train the policy.
+    flattened = {'observations': [], 'actions': []}
+    for path in expert_data:
+        for k in flattened.keys():
+            flattened[k].append(path[k])
+    for k in flattened.keys():
+        flattened[k] = np.concatenate(flattened[k])
+
+    obs_all = torch.from_numpy(flattened['observations']).float().to(device)
+    acs_all = torch.from_numpy(flattened['actions']).float().to(device)
+    N = obs_all.shape[0]
+
+    optimizer = optim.Adam(list(policy.parameters()), lr=1e-4)
+    idxs = np.array(range(len(expert_data)))
+    num_batches = len(idxs)*episode_length // batch_size
+    losses = []
+    for epoch in range(num_epochs):
+        np.random.shuffle(idxs)
+        running_loss = 0.0
+        for i in range(num_batches):
+            optimizer.zero_grad()
+            # TODO start: Fill in your behavior cloning implementation here, just maximize log likelihood!
+            # Sample a minibatch of (obs, action) pairs, compute the negative log-likelihood
+            # of the actions under the policy, and assign it to `loss`.
+            flattened_idxs = np.concatenate([
+                                    np.arange(idx * episode_length, (idx + 1) * episode_length) 
+                                    for idx in idxs
+            ])
+            batch_idxs = flattened_idxs[i * batch_size : (i + 1) * batch_size]
+
+            obs_batch = obs_all[batch_idxs]
+            acs_batch = acs_all[batch_idxs]
+
+            log_probs = policy.log_prob(obs_batch, acs_batch)
+            loss = -log_probs.mean()
+  
+            #loss = None
+            # TODO end
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        # if epoch % 10 == 0:
+        print('[%d] loss: %.8f' %
+            (epoch, running_loss / 10.))
+        #losses.append(loss.item())
+        losses.append(running_loss/num_batches)
+    return losses
---
+++ dagger.py
@@ -0,0 +1,87 @@
+"""
+TODO: MODIFY TO FILL IN YOUR DAGGER IMPLEMENTATION
+"""
+import torch
+import torch.optim as optim
+import numpy as np
+
+from utils import rollout, relabel_action
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+#device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+def simulate_policy_dagger(env, policy, expert_paths, expert_policy=None, num_epochs=500, episode_length=50,
+                            batch_size=32, num_dagger_iters=10, num_trajs_per_dagger=10):
+
+    # Fill in your dagger implementation in this function.
+
+    # Hint: Loop through num_dagger_iters iterations, at each iteration train a policy on the current dataset.
+    # Then rollout the policy, use relabel_action to relabel the actions along the trajectory with "expert_policy" and then add this to current dataset
+    # Repeat this so the dataset grows with states drawn from the policy, and relabeled actions using the expert.
+
+    # Optimizer code
+    optimizer = optim.Adam(list(policy.parameters()))
+    losses = []
+    returns = []
+
+    trajs = list(expert_paths)
+    # Dagger iterations
+    for dagger_itr in range(num_dagger_iters):
+        iter_losses = []
+        # Flatten current dataset for supervised training
+        flat_obs = np.concatenate([t['observations'] for t in trajs])
+        flat_ac = np.concatenate([t['actions'] for t in trajs])
+        obs_all = torch.from_numpy(flat_obs).float().to(device)
+        acs_all = torch.from_numpy(flat_ac).float().to(device)
+        N = obs_all.shape[0]
+
+        idxs = np.array(range(len(trajs)))
+        num_batches = len(idxs)*episode_length // batch_size
+        # Train the model with Adam
+        for epoch in range(num_epochs):
+            running_loss = 0.0
+            for i in range(num_batches):
+                optimizer.zero_grad()
+                # TODO start: Fill in your standard behavior cloning implementation here
+                # Sample a minibatch of (obs, action) pairs from the current aggregated dataset,
+                # compute the negative log-likelihood of the actions under the policy,
+                # and assign it to `loss`.
+                flattened_idxs = np.concatenate([
+                                     np.arange(idx * episode_length, (idx + 1) * episode_length) 
+                                     for idx in idxs
+                ])
+                batch_idxs = flattened_idxs[i * batch_size : (i + 1) * batch_size]
+
+                obs_batch = obs_all[batch_idxs]
+                acs_batch = acs_all[batch_idxs]
+
+                log_probs = policy.log_prob(obs_batch, acs_batch)
+                loss = -log_probs.mean()
+                #loss = None
+                # TODO end
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+            # if epoch % 10 == 0:
+            print('[%d, %5d] loss: %.8f' %(epoch + 1, i + 1, running_loss))
+            iter_losses.append(loss.item())
+        losses.append(iter_losses)
+
+        # Collecting more data for dagger
+        trajs_recent = []
+        for k in range(num_trajs_per_dagger):
+            env.reset()
+            # TODO start: Rollout the policy on the environment to collect more data, relabel them,
+            #             and then add them into trajs_recent
+            traj = rollout(env, policy, "dagger", episode_length=episode_length, render=False)
+            traj_relabel = relabel_action(traj, expert_policy)
+            trajs_recent.append(traj_relabel)
+            # TODO end
+
+        trajs += trajs_recent
+        mean_return = np.mean(np.array([traj['rewards'].sum() for traj in trajs_recent]))
+        print("Average DAgger return is " + str(mean_return))
+        returns.append(mean_return)
+    return losses, returns
